b7b97cfc4de20285c332ac01902d207bdb6f5262
don-briski01~ <abdulahiemoabino2@gmail.com>
2023-07-25 16:21:56 +0100

    implementation of tags for RTK query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,53 @@
-"use client"
+"use client";
 
 import CartList from "./components/cartList";
 import ProductList from "./components/product";
-import { useGetAllPostQuery } from "./store/slice/postApi";
-
+import {
+  useGetAllPostQuery,
+  useGetPostByIdQuery,
+  useCreatePostMutation,
+  useUpdatePostMutation,
+  useDeletePostMutation,
+} from "./store/slice/postApi";
 
 export default function Home() {
+  const [createPost, postResult] = useCreatePostMutation();
+  const [updatePost, updateResult] = useUpdatePostMutation();
+  const [deletePost, deleteResult] = useDeletePostMutation();
 
-  const data = useGetAllPostQuery("")
+  const data: any = useGetAllPostQuery("");
 
-  console.log("api data", data)
+  const singleData:any = useGetPostByIdQuery(6);
+
+  console.log("api data", data);
+  console.log("singlepost", singleData);
+  console.log("postResult", postResult)
+  console.log("updateResult", updateResult)
+  console.log("deleteResult", deleteResult)
 
   return (
     <main className=" p-5 items-center gap-10 flex flex-col">
-    <h1>Redux toolkit</h1>
-    
-    <ProductList />
+      <h1>Redux toolkit</h1>
 
-    <h1 className='mt-5'>Cart List</h1>
-    <CartList />
+      <div className='flex w-full items-center justify-evenly gap-10 flex-wrap'>
+        <h1>hello</h1>
+        <button onClick={() => createPost({ name: "bilal", id: "012" })}>
+          add post
+        </button>
+
+        <button onClick={() => updatePost({ name: "rantan lal", id: "119" })}>
+          update post
+        </button>
+
+        <button onClick={() => deletePost(119)}>
+          delete post
+        </button>
+      </div>
+
+      <ProductList />
+
+      <h1 className="mt-5">Cart List</h1>
+      <CartList />
     </main>
-  )
+  );
 }
--- src/app/store/slice/postApi.tsx
+++ src/app/store/slice/postApi.tsx
@@ -7,12 +7,69 @@ export const postApi = createApi({
     reducerPath: "postApi",
     baseQuery: fetchBaseQuery({baseUrl: "https://jsonplaceholder.typicode.com/" }),
     endpoints: (builder) => ({
+//endpoint to get all user
         getAllPost: builder.query({
             query: () => `users`
-        })
+        }),
+
+        getPostById: builder.query({
+            query: (id) => `users/${id}`
+        }),
+
+
+        //this is another method to perform a get request using mutation with RTK Query
+        getAllPosts: builder.mutation({
+            query:() => ({
+                url: `users`,
+                method: "GET",
+            })
+        }),
+
+        //this is  method to perform a Post request using mutation with RTK Query
+        createPost: builder.mutation({
+            query:(data) => ({
+                url: `users`,
+                method: "POST",
+                body: data
+            })
+        }),
+
+        //this is  method to perform a Put(update) request using mutation with RTK Query
+        updatePost: builder.mutation({
+            query:(data) => ({
+                url: `users/${data.id}`,
+                method: "PATCH",
+                body: data
+            })
+        }),
+
+           //this is  method to perform a delete request using mutation with RTK Query
+           deletePost: builder.mutation({
+            query:(id) => ({
+                url: `users/${id}`,
+                method: "DELETE",
+            
+            })
+        }),
+
+
+
+
+
+//endpoint to get a single user with params
+        //getSinglePost: builder.query({
+       //  query: (postId) => `user/${postId}`   
+      //  })
+
     })
 })
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetAllPostQuery } = postApi
+export const { useGetAllPostQuery, 
+    useGetPostByIdQuery, 
+    useCreatePostMutation,
+    useUpdatePostMutation,
+    useDeletePostMutation,
+ } = postApi
+
